c13c8b89b3b26faca0f137b8d78ca153ff06c0b4
MattiaSalnitri <mattia@Mattias-MacBook-Pro-3.local>
2016-11-30 16:22:55 +0100

    split functions

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function(request, response) 
+app.get('/select/', function(request, response) 
 {
 	var text = 'responce:';
 	response.writeHead(200, {'Content-Type': 'text/html'});
@@ -22,24 +22,6 @@ app.get('/', function(request, response)
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
 		
 		console.log("connected to db");
-	
-		/*create table	
-		client.query('create table test_table (id integer, name text)', function(err, result) {
-		  done();
-		  if (err)
-		   { console.error(err); response.send("Error " + err); }
-		  else
-		   { response.render('pages/db', {results: result.rows} ); }
-		});*/
-
-		/*/add element
-		client.query('insert into test_table values (1, \'hello database\')', function(err, result) {
-		  done();
-		  if (err)
-		   { console.error(err); response.send("Error insert " + err); }
-		  else
-		   { }
-		});*/
 
 		//query
 		client.query('SELECT * FROM test_table', function(err, result) {
@@ -60,6 +42,67 @@ app.get('/', function(request, response)
 		   }
 		});
 	    
+  	});
+  	
+	console.log("text: "+text);
+	response.end(text);
+
+});
+
+app.get('/create/', function(request, response) 
+{
+	var text = 'responce:';
+	response.writeHead(200, {'Content-Type': 'text/html'});
+	
+	console.log("called");
+
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+		
+		console.log("connected to db");
+	
+		//create table	
+		client.query('create table test_table (id integer, name text)', function(err, result) {
+		  done();
+		  if (err)
+		   { console.error(err); response.send("Error " + err); }
+		  else
+		   { response.render('pages/db', {results: result.rows} ); }
+		});
+
+		//add element
+		client.query('insert into test_table values (1, \'hello database\')', function(err, result) {
+		  done();
+		  if (err)
+		   { console.error(err); response.send("Error insert " + err); }
+		  else
+		   { }
+		});
+  	});
+  	
+	console.log("text: "+text);
+	response.end(text);
+
+});
+
+app.get('/add/', function(request, response) 
+{
+	var text = 'responce:';
+	response.writeHead(200, {'Content-Type': 'text/html'});
+	
+	console.log("called");
+
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+		
+		console.log("connected to db");
+
+		//add element
+		client.query('insert into test_table values (1, \'hello database\')', function(err, result) {
+		  done();
+		  if (err)
+		   { console.error(err); response.send("Error insert " + err); }
+		  else
+		   { }
+		});
   	});
   	
 	console.log("text: "+text);
